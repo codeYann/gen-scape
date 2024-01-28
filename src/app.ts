9b8cf78c18480d97b0d56843166a16605a305c6a
Yan Avlis <ccyan05@alu.ufc.br>
2024-01-28 10:30:46 -0300

    src/App.ts
    Running application.

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,24 @@
-console.log("Hello, World!");
+import { IPagination, Pagination } from "./pagination/pagination";
+
+class App {
+	app: IPagination;
+
+	constructor(pagination: IPagination) {
+		this.app = pagination;
+	}
+
+	async start() {
+		const iterator = this.app.getPaginated({
+			url: "https://www.mercadobitcoin.net/api/BTC/trades/",
+			page: 5704,
+		});
+
+		for await (const item of iterator) {
+			console.table(item);
+		}
+	}
+}
+
+const app = new App(new Pagination());
+
+app.start();
